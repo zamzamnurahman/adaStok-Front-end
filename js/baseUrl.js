afa4f34542c6cac0077498b7dfa2f4118c0f0c7f
zamzamnurahman <kazamnn1912@gmail.com>
2023-01-25 17:32:23 +0700

    fix base url

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://127.0.0.1:8000/api"
-export const baseUrl = "https://ada-stok-api.000webhostapp.com/public/api"
+export const baseUrl = "http://adastok.asgardiantekno.com/public/api"
