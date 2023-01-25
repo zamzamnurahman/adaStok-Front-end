3942cf3a28c11a14a06245ceda89316611f241b3
zamzamnurahman <kazamnn1912@gmail.com>
2023-01-25 18:08:06 +0700

    configure mode fetch

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://127.0.0.1:8000/api"
-export const baseUrl = "http://adastok.asgardiantekno.com/public/api"
+export const baseUrl = "http://kasirapi.site/public/api"
