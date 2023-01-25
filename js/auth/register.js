9e5eab0276c02e55fda57c0f7b10fc40e1afed6a
zamzamnurahman <kazamnn1912@gmail.com>
2023-01-25 18:29:42 +0700

    configure mode fetch

--- js/auth/register.js
+++ js/auth/register.js
@@ -28,8 +28,6 @@ async function daftarUser(name, email, password, confirm_password) {
 
   const response = await fetch(`${baseUrl}/register`, {
     method: 'POST',
-    mode: "no-cors",
-    credentials: "same-origin",
     headers: {
       "Content-Type": "application/json"
     },
